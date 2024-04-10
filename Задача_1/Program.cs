9598c834cd85731eb471144fe0bd7ef97cf6e660
Crazy Fly <citirin@list.ru>
2024-04-10 23:42:05 +0300

    Задача_1

---
+++ Задача_1/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+// Как задокументировать строку `cntrl+/`
+
+// Задача 1 - проверка на равенство числа 1 с квадратом числа 2
+Console.Write("Введите перове число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 == number2*number2)
+{
+    Console.WriteLine($"Квадрат числа {number2} равен {number1}");
+}
+else Console.WriteLine("Квадрат числа {0} не равен {1}", number1, number2);
+
+string number3 = Convert.ToString(Convert.ToInt32(Console.ReadLine()));
+Console.WriteLine(number3);
