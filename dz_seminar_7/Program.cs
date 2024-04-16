a81bbca6a020e757ba7685488a42d132f81dd6a5
Crazy Fly <citirin@list.ru>
2024-04-16 18:03:59 +0300

    Разкомитил ДЗ по семинару 7

--- dz_seminar_7/Program.cs
+++ dz_seminar_7/Program.cs
@@ -1,37 +1,37 @@
 ﻿//  Задание_1 Задайте значение m и n. Напишите программуб которая выведет все натуральные числа в промежутке от m до n. Использовать рекурсиюб не использовать циклы.
 
 
-// string Len(int stop,int start)
-// {
-//     if (start == stop) return Convert.ToString(stop);
-//     return start + "," + Len(stop,start +1);
-// }
+string Len(int stop,int start)
+{
+    if (start == stop) return Convert.ToString(stop);
+    return start + "," + Len(stop,start +1);
+}
 
-// Console.Write ("Введите число m: ");
-// int m = Convert.ToInt32(Console.ReadLine());
-// Console.Write ("Введите число n: ");
-// int n = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
 
-// if (m<n) Console.WriteLine(Len(n,m));
-// else Console.WriteLine(Len(m,n));
+if (m<n) Console.WriteLine(Len(n,m));
+else Console.WriteLine(Len(m,n));
 
 
 
 //  Задание_2 Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны 2 неотрицательных числа m и n.
 
-// Console.Write("Введите значение m1: ");
-// int m1 = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Введите значение n1: ");
-// int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение m1: ");
+int m1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение n1: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
 
-// int akkerman(int m1, int n1)
-// {
-//     if (m1 == 0) return n1 + 1;
-//     else if (n1 == 0) return akkerman(m1 - 1, 1);
-//     else return akkerman(m1 - 1, akkerman(m1, n1 - 1));
-// }
+int akkerman(int m1, int n1)
+{
+    if (m1 == 0) return n1 + 1;
+    else if (n1 == 0) return akkerman(m1 - 1, 1);
+    else return akkerman(m1 - 1, akkerman(m1, n1 - 1));
+}
 
-// Console.Write($"Функция Аккермана равно {akkerman(m1, n1)}");
+Console.Write($"Функция Аккермана равно {akkerman(m1, n1)}");
 
 
 
